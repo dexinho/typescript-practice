95da7220fdc2db7e8848357b09724618054cabe7
Jasmin Hidanovic <dexinho008@gmail.com>
2024-03-18 23:38:30 +0100

    added different types

--- practice.js
+++ practice.js
@@ -4,16 +4,13 @@ const calculateTaxes = ({ amount, rate, date, readOnly, }) => {
     if (amount > 1000)
         return amount * rate;
 };
+const identity = (arg) => {
+    return arg;
+};
+let output = identity(123); // Type of 'output' will be 'number'
 console.log(calculateTaxes({
     amount: 10000,
     rate: 0.1,
     date: new Date(),
     readOnly: false,
 }));
-const obj2 = {
-    amount: 1,
-    rate: 1,
-    date: new Date(),
-    readOnly: false,
-};
-obj2.amount = 10;
